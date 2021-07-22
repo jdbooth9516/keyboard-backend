7553e5c3364929ded42abffcf171e803affbe5bf
jeremiah booth <jeremiah.boothphotog@gmail.com>
2021-07-22 13:42:22 -0700

    init

---
+++ Keyboard/Keyboard/local_setting.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'Keyboards',
+        'USER': 'root',
+        'PASSWORD': 'hitokiri1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}   
---
+++ Keyboard/KeyboardAPI/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KeyboardapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'KeyboardAPI'
